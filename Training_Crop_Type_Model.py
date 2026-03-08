be653f8ebbbe521ec4af2bdd2f5fbc852c431d38
Soumik <shibmunchandra@gmail.com>
2026-03-09 00:56:29 +0530

    Label Encoded the df1

--- Training_Crop_Type_Model.py
+++ Training_Crop_Type_Model.py
@@ -3,3 +3,11 @@ import pandas as pd
 from Crop_Type import df
 
 df1=df.drop(['fertilizer_name'],axis=1)
+
+#Label Encoding the columns in df1
+from sklearn.preprocessing import LabelEncoder
+le1=LabelEncoder()
+le2=LabelEncoder()
+
+df1['soil_type']=le1.fit_transform(df['soil_type'])
+df1['crop_type']=le2.fit_transform(df['crop_type'])
