f9c0eb38c3743f49b16b18df14019eacb22d7780
Soumik <shibmunchandra@gmail.com>
2026-03-10 16:42:25 +0530

    Trained the model

--- Training_Crop_Type_Model.py
+++ Training_Crop_Type_Model.py
@@ -8,12 +8,21 @@ from sklearn.preprocessing import LabelEncoder
 le_crop=LabelEncoder()
 
 df['crop_type']=le_crop.fit_transform(df['crop_type'])
-print(df.head())
 
 #Defining X and y
-X=df.drop(df['crop_type'],axis=1)
+X=df.drop('crop_type',axis=1)
 y=df['crop_type']
 
 #Spliting X and y into train and test dataset
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+#Training the model
+from sklearn.ensemble import RandomForestClassifier
+rfc=RandomForestClassifier(n_estimators=100)
+
+rfc.fit(X_train,y_train)
+
+y_pred=rfc.predict(X_test)
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_pred,y_test))
