aa02d6830e5b162b02b5f924ff2f03f96ca0e521
Soumik <shibmunchandra@gmail.com>
2026-03-10 17:06:19 +0530

    Made function for crop prediction

--- Crop_Type.py
+++ Crop_Type.py
@@ -1,2 +1,15 @@
 import pandas as pd
+from Training_Crop_Type_Model import rfc
+from Training_Crop_Type_Model import le_crop
 
+def crop_pred(N,P,K,temp,humid,pH,rainfall):
+    input_data=[N,P,K,temp,humid,pH,rainfall]
+    input_features=['N','P','K','temperature','humidity','ph','rainfall']
+    
+    input_df=pd.DataFrame([input_data],columns=input_features)
+    y_pred=rfc.predict(input_df)
+    
+    crop_type=le_crop.inverse_transform(y_pred)
+    print(crop_type)
+    
+crop_pred(90,42,43,20.879744,82.002744,6.502985,202.935536)
