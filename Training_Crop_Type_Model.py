6e9c4dbed54a7dd9248e6ff58caa3e4cb8e8d19a
Soumik <shibmunchandra@gmail.com>
2026-03-10 16:35:10 +0530

    Train test split done

--- Training_Crop_Type_Model.py
+++ Training_Crop_Type_Model.py
@@ -1,30 +1,19 @@
 import pandas as pd
 
-from Crop_Type import df
+df=pd.read_excel('crop.xlsx')
 
-df1=df.drop(['fertilizer_name'],axis=1)
-
-#Label Encoding the columns in df1
+df=df.rename(columns={'label':'crop_type'})
+#Label Encoding the crop_type column
 from sklearn.preprocessing import LabelEncoder
-le1=LabelEncoder()
-le2=LabelEncoder()
+le_crop=LabelEncoder()
 
-df1['soil_type']=le1.fit_transform(df['soil_type'])
-df1['crop_type']=le2.fit_transform(df['crop_type'])
+df['crop_type']=le_crop.fit_transform(df['crop_type'])
+print(df.head())
 
-#Training the model
-X=df1.drop(['crop_type'],axis=1)
-y=df1['crop_type']
+#Defining X and y
+X=df.drop(df['crop_type'],axis=1)
+y=df['crop_type']
 
+#Spliting X and y into train and test dataset
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
-
-from sklearn.ensemble import RandomForestClassifier
-rfc=RandomForestClassifier(n_estimators=100)
-
-rfc.fit(X_train,y_train)
-
-y_pred=rfc.predict(X_test)
-
-from sklearn.metrics import accuracy_score
-accuracy=accuracy_score(y_test,y_pred)
